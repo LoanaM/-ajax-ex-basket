0b52005171bef6d199433b95c63809dfb39b7f38
LoanaWitch <loanawitch@live.com>
2018-04-17 20:02:23 +0200

    bozza yeah! it works!

--- ajbskmain.js
+++ ajbskmain.js
@@ -1,22 +1,31 @@
+players = []
+
 $('#submit').click(function(){
+ var k = $('#numberequest').val()
+ for (var i = 0; i < k; i++) {
+
 
     $.ajax ({
       url: 'https://www.boolean.careers/api/array/basket?n=1',
       method: "GET",
       success: function(data) {
-        codice = data.response[0].playerCode;
-        $('#play1').attr('placeholder',codice);
-        puntiplay1 = data.response[0].points
+
+        players.push(data.response[0])
+        // codice = data.response[0].playerCode;
+        // $('#play'+ i).attr('placeholder',codice);
+        // puntiplay1 = data.response[0].points
         console.log(data.response)
+        console.log(players)
       },
       error: function(){
         alert('error');
       }
     });
 
+ }
 })
 
-$(document).on('click','#play1',function(){
-
-  $('#punti').text(puntiplay1)
+$(document).on('click','#play0',function(){
+  var y = $(this).attr('num')
+  $('#punti').text(players[y].points)
 })
