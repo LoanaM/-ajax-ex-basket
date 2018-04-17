2f49dcced35a3904f0ae748ceb28019194d55c19
LoanaWitch <loanawitch@live.com>
2018-04-17 19:31:12 +0200

    only one call ok

--- ajbskmain.js
+++ ajbskmain.js
@@ -6,10 +6,17 @@ $('#submit').click(function(){
       success: function(data) {
         codice = data.response[0].playerCode;
         $('#play1').attr('placeholder',codice);
+        puntiplay1 = data.response[0].points
+        console.log(data.response)
       },
       error: function(){
         alert('error');
       }
     });
 
+})
+
+$(document).on('click','#play1',function(){
+
+  $('#punti').text(puntiplay1)
 })
